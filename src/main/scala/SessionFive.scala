0a483147d234b16e66384486c9b4b552afe2739d
Paul Matthews <paul.matthews@clearscore.com>
2023-03-09 13:56:51 +0000

    CS-000000: Session 5

---
+++ src/main/scala/SessionFive.scala
@@ -0,0 +1,34 @@
+object SessionFive extends App {
+  println("Session 5")
+
+  /**
+   * Exercise 1
+   *
+   * Cats - 🐈🐈‍⬛
+   *
+   * Create a class for Cat with fields for:
+   *
+   * - Name
+   * - Colour
+   * - Food
+   *
+   * And now create objects for the following cats
+   */
+
+
+
+
+
+
+
+  /** **Challenge**: Create a CreditCard class.
+    *
+    * It needs to store: cardName providerName creditLimit apr
+    *
+    * Now create card objects for:
+    *
+    * CapitalOne - credit limit £1099, apr 24.5 Amex - credit limit £5000, apr 34.0 Onmo - credit limit £500, apr 21.8
+    * Natwest - credit limit £800, apr 20.7 Get creative with the cardNames! i.e
+    */
+
+}
--- src/main/scala/SessionFour.scala
+++ src/main/scala/SessionFour.scala
@@ -1,13 +1,21 @@
 object SessionFour extends App {
   // ALTON TOWERS RIDES
-  val atOrOver90cm = "Runaway Mine Train, Gangsta Granny, Battle Galleons, Heave Ho, Enterprise, Raj's Bouncy Bottom Burp, Peter Rabbit Hippity Hop, Octonauts Rollercoaster Adventure, Justin's House Pie-O-Matic Factory, Go Jetters Vroomster Zoom Ride, Duel: The Haunted House Strikes Back"
+  val atOrOver90cm =
+    "Runaway Mine Train, Gangsta Granny, Battle Galleons, Heave Ho, Enterprise, Raj's Bouncy Bottom Burp, Peter Rabbit Hippity Hop, Octonauts Rollercoaster Adventure, Justin's House Pie-O-Matic Factory, Go Jetters Vroomster Zoom Ride, Duel: The Haunted House Strikes Back"
   val atOrOver100cm = "The Blade"
   val atOrOver110cm = "Marauder's Mayhem, Congo River Rapids"
   val atOrOver120cm = "TH13TEEN, Spinball Whizzer, Wicker Man"
   val atOrOver140cm = "Rita  Maximum, Galactica, The Smiler, Oblivion"
 
   def heightLimit(height: Double): String = {
-    val rides = Array("Rita, Galactica, The Smiler, Oblivion", "TH13TEEN, Spinball Whizzer, Wicker Man", "Marauder's Mayhem, Congo River Rapids", "The Blade", "Gangsta Granny: The Ride, Runaway Mine Train, Battle Galleons, Heave Ho, Enterprise, Raj's Bouncy Bottom Burp, Peter Rabbit Hippity Hop, Octonauts Rollercoaster Adventure, Justin's House Pie-O-Matic Factory, Go Jetters Vroomster Zoom Ride, Duel: The Haunted House Strikes Back")
+    val rides =
+      Array(
+        "Rita, Galactica, The Smiler, Oblivion",
+        "TH13TEEN, Spinball Whizzer, Wicker Man",
+        "Marauder's Mayhem, Congo River Rapids",
+        "The Blade",
+        "Gangsta Granny: The Ride, Runaway Mine Train, Battle Galleons, Heave Ho, Enterprise, Raj's Bouncy Bottom Burp, Peter Rabbit Hippity Hop, Octonauts Rollercoaster Adventure, Justin's House Pie-O-Matic Factory, Go Jetters Vroomster Zoom Ride, Duel: The Haunted House Strikes Back"
+      )
     if (height >= 140.0) "You can ride everything!"
     else if (height >= 120.0) "You can jump on: " + (rides(4) ++ rides(3) ++ rides(2) ++ rides(1))
     else if (height >= 110.0) "You can jump on: " + (rides(3) ++ rides(2) ++ rides(1))
@@ -15,20 +23,13 @@ object SessionFour extends App {
     else "You can jump on: " + rides(1)
   }
 
-
-
   def greeting(name: String, age: Int): String = {
     s"Hi my name is $name and I am $age years old"
   }
   // input: Double
   // output: String
 
-
-  def greeting(name: String, age: Int): String = {
-    s"Hi my name is $name and I am $age years old"
-  }
+//  def greeting(name: String, age: Int): String = {
+//    s"Hi my name is $name and I am $age years old"
+//  }
 }
-
-
-
-assert(height())
